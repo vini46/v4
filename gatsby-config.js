7c4dee4f6cea46e695225a43d17641a15b11e570
vinod <vinod.baradwaj@live.com>
2021-05-05 17:18:57 +0530

    new changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
   siteMetadata: {
     title: 'Vinod Baradwaj',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+      'Vinod is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
     siteUrl: 'https://vinodvb.in', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@vbvini',
